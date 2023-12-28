b4f7487a5f4191dc3191e6267468dca4f851efd1
Adrian_George <adrian_george28@yahoo.com>
2023-12-28 10:44:42 +0000

    Add alternate character and word case transformation

--- alternative.py
+++ alternative.py
@@ -1,3 +1,4 @@
+# Alternate Character Case Transformation
 # Ask the user to enter a string
 user_input = input("Enter a string: ")
 
@@ -15,3 +16,24 @@ for i in range(len(user_input)):
 
 # Print the result
 print("Transformedd String:", result)
+
+
+
+# Alternate Word Case Transformation
+user_input = "Happy New Year"
+user_input = user_input.split()
+
+# Initialize an empty string to store the result
+result = ""
+
+for i in range(len(user_input)):
+
+    if i % 2 == 0:
+     
+        result += user_input[i].upper() + " "
+    else:
+      
+        result += user_input[i].lower() + " "
+
+# Print the result
+print("Transformed String :", result.strip())
